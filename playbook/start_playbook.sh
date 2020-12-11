8ffa21f3a879d6e12a625df99e0a875e2136c2b5
Andrey Kaa <syatihoko@mail.ru>
2020-12-11 20:26:27 +0300

    Ready

---
+++ playbook/start_playbook.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash  
+
+#list of hosts
+pc_names=("centos7" "ubuntu" "fedora")
+
+
+
+
+start_docker ()
+{
+for i in ${pc_names[@]}
+do
+    #Если в конце число
+    if (echo "$i" | grep -E -q "^?[0-9]+$"); then
+     version=${i: -1}
+     echo version = $version
+     os_name=${i:0: -1}
+     echo os_name = $os_name
+
+    #Создаем Docker-контейнер
+     docker run --name kaa_$i -d pycontribs/$os_name:$version sleep 60000
+
+    else
+     version=latest
+     echo version = $version
+     os_name=$i
+
+    #Создаем Docker-контейнер     
+     docker run --name kaa_$i -d pycontribs/$os_name:$version sleep 60000
+    fi
+done
+}
+
+
+stop_docker ()
+{
+for i in ${pc_names[@]}
+do
+    docker stop kaa_$i
+    docker rm kaa_$i
+done
+}
+
+start_docker
+
+echo netology > temp.txt
+ansible-playbook -i inventory/prod.yml site.yml --vault-password-file temp.txt
+rm -f temp
+
+
+stop_docker
+
